40562550badde675bb3a0c5d25458c78b3d93b97
sakhibjamal <osakhibjamal@gmail.com>
2022-03-02 16:02:19 +0500

    comment1

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container mt-5">
+                <div className="d-flex flex-column align-items-center">
+                    <h1>Олег Марков</h1>
+                    <ul className="d-flex list-unstyled gap-3  ">
+                        <li><a className="text-dark text-decoration-none" href="#">Главная</a></li>
+                        <li><a className="text-secondary text-decoration-none" href="#">Обо мне</a></li>
+                        <li><a className="text-secondary text-decoration-none" href="#">Связаться</a></li>
+                    </ul>
+                </div>
+
+                <div className="row px-2">
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <img className="img-fluid"
+                            src="https://static.wixstatic.com/media/d78df6_4517dc1e0b1545bf84dccd915203c870.jpg/v1/fill/w_344,h_344,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_4517dc1e0b1545bf84dccd915203c870.jpg"
+                            alt=""/>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <img className="img-fluid"
+                            src="https://static.wixstatic.com/media/d78df6_9f4f79dd27814710b261e3acb931156e.jpg/v1/fill/w_229,h_344,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_9f4f79dd27814710b261e3acb931156e.jpg"
+                            alt=""/>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <img className="img-fluid"
+                            src="https://static.wixstatic.com/media/d78df6_914d12d64a57420c99737b9933ebe7fc.jpg/v1/fill/w_344,h_344,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_914d12d64a57420c99737b9933ebe7fc.jpg"
+                            alt=""/>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <img className="img-fluid"
+                            src="https://static.wixstatic.com/media/d78df6_3eea4c192eed4667a602ead5652459c5.jpg/v1/fill/w_344,h_344,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_3eea4c192eed4667a602ead5652459c5.jpg"
+                            alt=""/>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <img className="img-fluid"
+                            src="https://static.wixstatic.com/media/d78df6_afe9335eb67640c988d648f7671308e5.jpg/v1/fill/w_344,h_344,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_afe9335eb67640c988d648f7671308e5.jpg"
+                            alt=""/>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <img className="img-fluid"
+                            src="https://static.wixstatic.com/media/d78df6_b572be78b8984e2db58978eb21e433d7.jpg/v1/fill/w_344,h_344,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_b572be78b8984e2db58978eb21e433d7.jpg"
+                            alt=""/>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <img className="img-fluid"
+                            src="https://static.wixstatic.com/media/d78df6_b9747effc3074458a295cf5674f8c73c.jpg/v1/fill/w_344,h_344,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_b9747effc3074458a295cf5674f8c73c.jpg"
+                            alt=""/>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <img className="img-fluid"
+                            src="https://static.wixstatic.com/media/d78df6_24dc2b5011604bc7ada79cbd953a7c5a.jpg/v1/fill/w_344,h_344,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_24dc2b5011604bc7ada79cbd953a7c5a.jpg"
+                            alt=""/>
+                    </div>
+                    <div className="col-12 col-md-6 col-lg-4">
+                        <img className="img-fluid"
+                            src="https://static.wixstatic.com/media/d78df6_394a136048754355ad8432b30522c761.jpg/v1/fill/w_344,h_344,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/d78df6_394a136048754355ad8432b30522c761.jpg"
+                            alt=""/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
